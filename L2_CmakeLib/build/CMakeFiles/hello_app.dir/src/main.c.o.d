9325eb45add9f2df24d6fee301e59b77deab41bd
Dominik <zawlockidominik@gmail.com>
2025-03-10 21:29:06 +0100

    Create files for Cmake Library

---
+++ L2_CmakeLib/build/CMakeFiles/hello_app.dir/src/main.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/hello_app.dir/src/main.c.o: \
+ /media/dom/Wkspce/GitRepos/algorithms/L2_CmakeLib/src/main.c \
+ /usr/include/stdc-predef.h \
+ /media/dom/Wkspce/GitRepos/algorithms/L2_CmakeLib/src/hello.h
