0ad808bc331801eb0f41ebff282bee6cc31d0e42
Dominik Zawlocki <zawlockidominik@gmail.com>
2025-05-08 19:25:57 +0200

    Simplify list1.hs

--- Haskell/L2_lists/lists1.hs
+++ Haskell/L2_lists/lists1.hs
@@ -1,3 +1,4 @@
+import Data.Char (ord)
 
 listToStr :: [Int] -> String
 listToStr [] = ""
@@ -11,21 +12,12 @@ floatListToStr [x] = show x
 floatListToStr (x:xs) = (show x) ++ " " ++ (floatListToStr xs)
 
 
-reverseList [] = []
-reverseList (x:xs) = reverseList xs ++ [x]
-
-toChar :: Int -> Char
-toChar = toEnum
-
-
--- Funkcja main
 main :: IO ()
 main = do
-    input <- getLine
-    let n = read input :: Int
-    putStrLn $ listToStr [n..(n+20)]
-    putStrLn $ listToStr (reverseList ([(n+2)..(n+15)]))
-    let nFloat = fromIntegral n :: Float
-    putStrLn $ floatListToStr [(fromIntegral n :: Float) + (i * 0.125) | i <- [0..(16 * 8)]]
-    putStrLn $ show $ [toChar (97 + i) | i <- [0..25], i `mod` 2 == 0]
-    
+  input <- getLine
+  let n = read input :: Int
+  putStrLn $ listToStr [n..(n+20)]
+  putStrLn $ listToStr [(n+15), (n+14)..(n+2)]
+  let nFloat = fromIntegral n :: Float
+  putStrLn $ floatListToStr [nFloat, nFloat+0.125 .. nFloat+16.0]
+  putStrLn $ show $ [c | c <- ['a'..'z'], ord c `mod` 2 /= 0]
