56c74c0bcbc71039538712de7243a552be55512b
Dominik <zawlockidominik@gmail.com>
2025-05-08 10:51:45 +0200

    Adjust game_of_life code for auto-checker

--- L6_GameOfLife/src/game_of_life.h
+++
@@ -1,7 +0,0 @@
-#include <stdbool.h>
-
-void simulate(bool** tab, int w, int h);
-
-void print_map(bool** tab, int w, int h);
-
-void fill_map_randomly(bool* tab[10], int w, int h, int cells_count);
--- L6_GameOfLife/src/main.c
+++ L6_GameOfLife/src/main.c
@@ -2,12 +2,116 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <unistd.h>
 
-#include "game_of_life.h"
+
+int max(int a, int b) {
+	return a > b ? a : b;
+}
+
+int min(int a, int b) {
+	return a < b ? a : b;
+}
+
+unsigned int count_neighbours(bool** tab, int w, int h, int cx, int cy) {
+	unsigned int count = 0;
+
+	for (int y = max(cy-1, 0); y <= min(cy+1, h-1); y++) {
+		for (int x = max(cx-1, 0); x <= min(cx+1, w-1); x++) {
+			if (x == cx && y == cy) {
+				// It's THIS cell - ignore it!
+				continue;
+			}
+			if (tab[x][y]) {
+				count++;
+			}
+		}
+	}
+
+	return count;
+}
+
+void simulate(bool** tab, int w, int h) {
+	bool** tab_next_gen;
+	tab_next_gen = malloc(w * h * sizeof(bool));
+	for (int i = 0; i < w; i++) {
+		tab_next_gen[i] = malloc(h * sizeof(bool));
+	}
+
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
+			unsigned int neighbours = count_neighbours(tab, w, h, x, y);
+
+			if (tab[x][y]) {
+				// Alive!
+				if (neighbours == 2 || neighbours == 3) {
+					tab_next_gen[x][y] = true;
+				} else {
+					tab_next_gen[x][y] = false;
+				}
+			}else {
+				// Dead :c
+				if (neighbours == 3) {
+					tab_next_gen[x][y] = true;
+				}else {
+					tab_next_gen[x][y] = false;
+				}
+			}
+		}
+	}
+
+	// Rewrite
+	//	memcpy(tab, tab_next_gen, w * h * sizeof(bool));
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
+			tab[x][y] = tab_next_gen[x][y];
+		}
+	}
+
+	for (int i = 0; i < w; i++) {
+		free(tab_next_gen[i]);
+	}
+	free(tab_next_gen);
+}
+
+void print_map(bool** tab, int w, int h) {
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
+			printf("%s ", tab[x][y] ? "O" : ".");
+		}
+		printf("\n");
+	}
+}
+
+
+void fill_map_randomly(bool** tab, int w, int h, int cells_count) {
+	// Clear map first
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
+			tab[x][y] = false;
+		}
+	}
+
+	for (int i = 0; i < cells_count; i++) {
+		int x = rand() % w;
+		int y = rand() % h;
+
+		if (tab[x][y]) {
+			// Cell is already in here
+			i--;
+			continue;
+		}
+
+		tab[x][y] = true;
+	}
+}
 
 
 int main(int argc, char** argv) {
-	srand(time(NULL));
+//	srand(time(NULL));
+	srand(1234);
+	usleep(500000);
 
 	const int W = 20;
 	const int H = 10;
@@ -16,7 +120,7 @@ int main(int argc, char** argv) {
 	int cells_count = 50;
 	int iterations = 5;
 
-	printf("Podaj liczbę początkowych żywych komórek: ");
+	printf("Podaj liczbę początkowych żywych komórek: \n");
 	scanf("%d", &cells_count);
 
 	printf("Podaj liczbę lat (iteracji): ");
@@ -29,12 +133,12 @@ int main(int argc, char** argv) {
 
 	fill_map_randomly(tab, W, H, cells_count);
 
-	printf("\nRok %d/%d:\n", 0, iterations);
+	printf("\nRok %d:\n", 0);
 	print_map(tab, W, H);
 
 	for (int i = 0; i < iterations; i++) {
 		simulate(tab, W, H);
-		printf("\nRok %d/%d:\n", i+1, iterations);
+		printf("Rok %d:\n", i+1);
 		print_map(tab, W, H);
 	}
 
