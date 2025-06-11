667dee350e3f7040af74dfc10ead579ae2115ca6
Dominik <zawlockidominik@gmail.com>
2025-06-11 21:21:05 +0200

    Counting neighbours in rows

--- Haskell/HaskellOfLife/main.hs
+++ Haskell/HaskellOfLife/main.hs
@@ -7,6 +7,26 @@ showWorld (w:rest) = do
     showWorld rest
 
 
+countIfAlive :: Char -> Int
+countIfAlive '#' = 1
+countIfAlive _ = 0
+
+countInRow :: String -> String -> Int
+countInRow [] right = (countIfAlive $ head right)
+countInRow left [] = (countIfAlive $ last left)
+countInRow left right = (countIfAlive $ last left) + (countIfAlive $ head right)
+
+countWholeRow :: String -> [Int]
+countWholeRow row = [(countInRow (take (x-1) row) (drop x row)) | x <- [1..5]]
+
+countNeighbours :: [String] -> [[Int]]
+countNeighbours world =
+    [(countWholeRow r) | r <- world]
+
+    
+   
+    
+
 main :: IO ()
 main = do
     let width = 5
@@ -17,6 +37,7 @@ main = do
     let world = [".####", "#.###", "##.##", "###.#", "####."]
 
     showWorld world
-    
+    let foo = countNeighbours world
+    putStrLn $ show foo
     putStrLn "END!"
 
