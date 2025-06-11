d0c0e51048dfc5df83d783e19f07a23fde5ecafe
Dominik <zawlockidominik@gmail.com>
2025-06-11 21:58:53 +0200

    Properly count neighbours for whole array

--- Haskell/HaskellOfLife/main.hs
+++ Haskell/HaskellOfLife/main.hs
@@ -1,4 +1,7 @@
 
+getElement :: (Ord a) => [a] -> Int -> a
+getElement elements idx = head (drop (idx-1) elements)
+
 showWorld :: [String] -> IO ()
 showWorld [] = putStrLn ""
 showWorld [a] = putStrLn a
@@ -6,6 +9,11 @@ showWorld (w:rest) = do
     putStrLn w  
     showWorld rest
 
+add2Lists :: [Int] -> [Int] -> [Int]
+add2Lists ta tb = [(fst x + snd x) | x <- (zip ta tb)] 
+
+add3Lists :: [Int] -> [Int] -> [Int] -> [Int]
+add3Lists a b c = add2Lists a $ add2Lists b c
 
 countIfAlive :: Char -> Int
 countIfAlive '#' = 1
@@ -17,15 +25,20 @@ countInRow left [] = (countIfAlive $ last left)
 countInRow left right = (countIfAlive $ last left) + (countIfAlive $ head right)
 
 countWholeRow :: String -> [Int]
+countWholeRow [] = [0, 0, 0, 0, 0]
 countWholeRow row = [(countInRow (take (x-1) row) (drop x row)) | x <- [1..5]]
 
-countNeighbours :: [String] -> [[Int]]
-countNeighbours world =
-    [(countWholeRow r) | r <- world]
+countWholeRowIncludingThemself :: String -> [Int]
+countWholeRowIncludingThemself [] = [0, 0, 0, 0, 0]
+countWholeRowIncludingThemself row = [(countInRow (take (x-1) row) (drop x row)) + (countIfAlive (getElement row x)) | x <- [1..5]]
 
-    
-   
-    
+countNeighbours :: [String] -> [[Int]]
+countNeighbours world = 
+    let first = add2Lists (countWholeRow $ getElement world 1) (countWholeRowIncludingThemself $ getElement world 2)
+        middle = [add3Lists (countWholeRowIncludingThemself $ getElement world (idx-1)) (countWholeRow $ getElement world idx) (countWholeRowIncludingThemself $ getElement world (idx+1)) | idx <- [2..4]]
+        last = add2Lists (countWholeRowIncludingThemself $ getElement world 4) (countWholeRow $ getElement world 5)
+    in [first] ++ middle ++ [last]
+
 
 main :: IO ()
 main = do
@@ -34,7 +47,7 @@ main = do
 
     putStrLn $ "World dimensions: " ++ (show width) ++ " x " ++ (show height)
 
-    let world = [".####", "#.###", "##.##", "###.#", "####."]
+    let world = ["#####", "#.###", "##.##", "###.#", "#####"]
 
     showWorld world
     let foo = countNeighbours world
