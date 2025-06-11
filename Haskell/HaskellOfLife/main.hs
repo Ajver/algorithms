324600cbeefdb3f5f74510eb69c719128ff9a820
Dominik <zawlockidominik@gmail.com>
2025-06-11 23:02:07 +0200

    use constant width and add new world

--- Haskell/HaskellOfLife/main.hs
+++ Haskell/HaskellOfLife/main.hs
@@ -1,3 +1,5 @@
+width = 5
+
 -- helper func 
 getElement :: (Ord a) => [a] -> Int -> a
 getElement elements idx = head (drop (idx-1) elements)
@@ -24,23 +26,25 @@ countIfAlive :: Char -> Int
 countIfAlive state = if state == '#' then 1 else 0
 
 countNeighbourForCellInOneRow :: String -> Int -> Int
-countNeighbourForCellInOneRow row 1 = (countIfAlive $ getElement row 2)
-countNeighbourForCellInOneRow row 5 = (countIfAlive $ getElement row 4)
-countNeighbourForCellInOneRow row idx = (countIfAlive $ getElement row (idx - 1)) + (countIfAlive $ getElement row (idx + 1))
+countNeighbourForCellInOneRow row idx
+    | idx == 1 = (countIfAlive $ getElement row 2)
+    | idx == width = (countIfAlive $ getElement row 4)
+    | otherwise = (countIfAlive $ getElement row (idx - 1)) + (countIfAlive $ getElement row (idx + 1))
 
 countNeigboursInRow :: String -> [Int]
-countNeigboursInRow row = [countNeighbourForCellInOneRow row x | x <- [1 .. 5]]
+countNeigboursInRow row = [countNeighbourForCellInOneRow row x | x <- [1 .. width]]
 
 countNeighboursInRowIncludingThemselves :: String -> [Int]
-countNeighboursInRowIncludingThemselves row = [countNeighbourForCellInOneRow row x + (countIfAlive (getElement row x)) | x <- [1..5]]
+countNeighboursInRowIncludingThemselves row = [countNeighbourForCellInOneRow row x + (countIfAlive (getElement row x)) | x <- [1..width]]
 
 countRowAndSurrounding :: [String] -> Int -> [Int]
-countRowAndSurrounding world 0 = add2Lists (countNeigboursInRow $ getElement world 1) (countNeighboursInRowIncludingThemselves $ getElement world 2)
-countRowAndSurrounding world 5 = add2Lists (countNeighboursInRowIncludingThemselves $ getElement world 4) (countNeigboursInRow $ getElement world 5)
-countRowAndSurrounding world rowIdx = add3Lists (countNeighboursInRowIncludingThemselves $ getElement world (rowIdx-1)) (countNeigboursInRow $ getElement world rowIdx) (countNeighboursInRowIncludingThemselves $ getElement world (rowIdx+1))
+countRowAndSurrounding world rowIdx
+    | rowIdx == 1 = add2Lists (countNeigboursInRow $ getElement world 1) (countNeighboursInRowIncludingThemselves $ getElement world 2)
+    | rowIdx == width = add2Lists (countNeighboursInRowIncludingThemselves $ getElement world 4) (countNeigboursInRow $ getElement world width)
+    | otherwise = add3Lists (countNeighboursInRowIncludingThemselves $ getElement world (rowIdx-1)) (countNeigboursInRow $ getElement world rowIdx) (countNeighboursInRowIncludingThemselves $ getElement world (rowIdx+1))
 
 countNeighbours :: [String] -> [[Int]]
-countNeighbours world = [countRowAndSurrounding world idx | idx <- [1..5]]
+countNeighbours world = [countRowAndSurrounding world idx | idx <- [1..width]]
 
 al :: Char -> Int -> Char
 al state count
@@ -73,19 +77,18 @@ live_Cycyle world n = do
 
 
 logic :: [String] -> [[Int]] -> [String]
-logic world counts = [applyLogicForRow (getElement world idx) (getElement counts idx) | idx <- [1..5]]
+logic world counts = [applyLogicForRow (getElement world idx) (getElement counts idx) | idx <- [1..width]]
 
 main :: IO ()
 main = do
-    let width = 5
     let height = 5
     let epoch = 10
     putStrLn $ "World dimensions: " ++ (show width) ++ " x " ++ (show height)
 
     let world = [".....", ".....", ".###.", ".....", "....."]
-
+    let world2 = ["..#..", ".#.#.", "..#..", "##.##", ".#.#."]
     
-    live_Cycyle world epoch
+    live_Cycyle world2 epoch
 
     
 
