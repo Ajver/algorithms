af34638d4d89f3d3f6022f616a18e02ac98c51c4
Dominik <zawlockidominik@gmail.com>
2025-11-18 21:56:33 +0100

    better plots

--- OS/Lab6-threads/draw_measurements_charts.py
+++ OS/Lab6-threads/draw_measurements_charts.py
@@ -3,10 +3,18 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_csv("measurements.csv")
 
-plt.scatter(dataset["t"], dataset["duration"])
-plt.title(f"n = {dataset['n'][0]}, k = {dataset['k'][0]}")
+unique_t = dataset["t"].unique()
+durations_categorized = {t: dataset["duration"][dataset["t"] == t] for t in unique_t}
+duration_means = {t: durations_categorized[t].mean() for t in unique_t}
 
-plt.xlabel("Number of Threads (t)")
-plt.ylabel("Duration (seconds)")
+fig, ax = plt.subplots()
+ax.set_title(f"n = {dataset['n'][0]}, k = {dataset['k'][0]}")
+
+ax.plot(duration_means.keys(), duration_means.values(), alpha=0.5)
+ax.boxplot(durations_categorized.values(), meanline=True, showmeans=True)
+ax.set_xticklabels(durations_categorized.keys())
+
+ax.set_xlabel("Number of Threads (t)")
+ax.set_ylabel("Duration (seconds)")
 
 plt.savefig("performance.png") 
