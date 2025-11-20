14c42ba21501f9d86562e23ee54e2f87a3aa47a6
Dominik <zawlockidominik@gmail.com>
2025-11-20 12:04:20 +0100

    Measure 1-4 t

--- OS/Lab6-threads/draw_measurements_charts.py
+++ OS/Lab6-threads/draw_measurements_charts.py
@@ -1,11 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 dataset = pd.read_csv("measurements.csv")
 
 unique_t = dataset["t"].unique()
 durations_categorized = {t: dataset["duration"][dataset["t"] == t] for t in unique_t}
 duration_means = {t: durations_categorized[t].mean() for t in unique_t}
+
+# fit LSE line
+model = LinearRegression()
+X = np.array(dataset["t"])
+y = dataset["duration"]
+result = model.fit(X.reshape(-1, 1), y)
+print(f"{result.coef_=}")
+
+b0 = result.intercept_
+b1 = result.coef_[0]
 
 fig, ax = plt.subplots()
 ax.set_title(f"n = {dataset['n'][0]}, k = {dataset['k'][0]}")
@@ -14,7 +26,15 @@ ax.plot(duration_means.keys(), duration_means.values(), alpha=0.5)
 ax.boxplot(durations_categorized.values(), meanline=True, showmeans=True)
 ax.set_xticklabels(durations_categorized.keys())
 
+xlim = ax.get_xlim()
+A, B = (xlim[0], b0 + b1*xlim[0]), (xlim[1], b0 + b1*xlim[1])
+ax.axline((xlim[0], b0 + b1*xlim[0]), (xlim[1], b0 + b1*xlim[1]), c="r", linestyle="--", label=f"{b0} + {b1}*t")
+
 ax.set_xlabel("Number of Threads (t)")
 ax.set_ylabel("Duration (seconds)")
 
+ax.legend()
+
 plt.savefig("performance.png") 
+
+
--- OS/Lab6-threads/run_multiple_times.sh
+++ OS/Lab6-threads/run_multiple_times.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-n=3000
-k=50000
+n=1000
+k=1200
 
 
-for i in {1..5}; do
-    for t in {1..10}; do
+for i in {1..20}; do
+    for t in {1..4}; do
         ./psum $n $k $t
     done
 done
