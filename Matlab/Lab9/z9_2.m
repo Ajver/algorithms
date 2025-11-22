964e46ab957a78eac05294f6de4d04e73613ddb4
Dominik <zawlockidominik@gmail.com>
2025-11-22 20:23:28 +0100

    Improve results displaying

--- Matlab/Lab9/z9_2.m
+++ Matlab/Lab9/z9_2.m
@@ -2,16 +2,21 @@
 clear all; close all;
 
 it = 12;
-% a  = pi-pi/4; b=pi+pi/5;  % znajdz zero funkcji y=sin(x) dla x=pi
-% f  = @(x) sin(x);         % definicja funkcji
-% fp = @(x) cos(x);         % definicja pochodnej funkcji
-% z = pi
 
-nachylenie_okolo = 10;
+%%
+a  = pi-pi/5; b=pi+pi/5;  % znajdz zero funkcji y=sin(x) dla x=pi
+f  = @(x) sin(x);         % definicja funkcji
+fp = @(x) cos(x);         % definicja pochodnej funkcji
+z = pi
+TOL = 0.001 * pi;  % 1 promil pi
+
+
+%%
+nachylenie_okolo = 45;
 
 switch nachylenie_okolo
     case 10
-        a = -4; b = 5;
+        a = -1; b = 5;
         A = 0.15; B = 0.3; C = 0.1;  
     case 45
         a = -4; b = 1;
@@ -29,7 +34,9 @@ z2 = (-B + sqrt(delta)) / (2*A);
 z = z2
 kat_paraboli_w_z = rad2deg(atan(fp(z)))
 
+TOL = 0.001;
 
+%%
 x = -10 : 0.01 : 10;
 figure; plot( x, f(x), 'b-', x, fp(x),'r-'); grid; xlabel('x'); title('f(x), fp(x)');
 legend('Funkcja','Jej pochodna');
@@ -37,12 +44,17 @@ legend('Funkcja','Jej pochodna');
 cb = nonlinsolvers( f, fp, a, b, 'bisection', it );
 cr = nonlinsolvers( f, fp, a, b, 'regula-falsi', it);
 cn = nonlinsolvers( f, fp, a, b, 'newton-raphson', it);
-figure; plot( 1:it,cb,'o-', 1:it,cr,'*-', 1:it,cn,'^-'); xlabel('iter'); title('c(iter)');
+figure;     
+    plot( 1:it,cb,'o-' ); hold on;
+    plot( 1:it,cr,'*-' );
+    plot( 1:it,cn,'^-' );
+
+xlabel('iter'); title('c(iter)');
 grid on, legend('Bisection','Regula-Falsi','Newton-Raphson');
 
-dok = 0.001;
+kiedy_zbierzne = @(preds) min(find(abs(preds - z) <= TOL));
 
-fprintf('Bisection it: %d\n', min(find(abs(cb - z)/pi <= dok)));
-fprintf('Regula-Falsi method result: %d\n', min(find(abs(cr - z)/pi <= dok)));
-fprintf('Newton-Raphson method result: %d\n', min(find(abs(cn - z)/pi <= dok)));
+fprintf('Bisection it: %d, result: %f, error: %f\n', kiedy_zbierzne(cb), cb(end), cb(end) - z);
+fprintf('Regula-Falsi it: %d, result: %f, error: %f\n', kiedy_zbierzne(cr), cr(end), cr(end) - z);
+fprintf('Newton-Raphson it: %d, result: %f, error: %f\n', kiedy_zbierzne(cn), cn(end), cn(end) - z);
 
