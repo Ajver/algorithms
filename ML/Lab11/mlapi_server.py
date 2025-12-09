1593cdeb2874b3714e511c3a7bb1d40b3b8b24e0
Dominik <zawlockidominik@gmail.com>
2025-12-09 22:38:16 +0100

    Add example request body to test mlapi server

--- ML/Lab11/mlapi_server.py
+++ ML/Lab11/mlapi_server.py
@@ -6,6 +6,18 @@ import numpy as np
 
 COLUMNS = ['radius', 'texture', 'area', 'smoothness', 'compactness', 'symmetry']
 
+"""  Example request body
+{
+	"data": [
+        [18,13,409,0.095,0.055,0.192],
+        [11,11,466,0.088,0.094,0.193],
+        [10,24,645,0.105,0.187,0.225],
+        [11,11,1104,0.091,0.219,0.231],
+        [17,21,503,0.098,0.052,0.159]
+	]
+}
+"""
+
 
 class MLAPIServer(MLController):
     model_name = "my-classifier"
