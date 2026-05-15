a63d518c65c967ad4dd65c8df215afeed7ee230a
Dominik <zawlockidominik@gmail.com>
2026-05-15 18:57:59 +0200

    Add missing zero_grad and fix train/val datasets being the same

--- DL/Lab8/SpeachClassifierModel.py
+++ DL/Lab8/SpeachClassifierModel.py
@@ -6,29 +6,33 @@ class SpeachClassifierModel(nn.Module):
         super().__init__()
 
         self.model = nn.Sequential(
-            nn.Conv1d(in_channels=1, out_channels=8, kernel_size=13, padding="valid"),
+            nn.Conv1d(in_channels=1, out_channels=8, kernel_size=65, padding=32),
+            nn.BatchNorm1d(8),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=3),
             nn.Dropout(dropout_rate),
 
-            nn.Conv1d(in_channels=8, out_channels=16, kernel_size=11, padding="valid"),
+            nn.Conv1d(in_channels=8, out_channels=16, kernel_size=11, padding=5),
+            nn.BatchNorm1d(16),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=3),
             nn.Dropout(dropout_rate),
 
-            nn.Conv1d(in_channels=16, out_channels=32, kernel_size=9, padding="valid"),
+            nn.Conv1d(in_channels=16, out_channels=32, kernel_size=7, padding=3),
+            nn.BatchNorm1d(32),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=3),
             nn.Dropout(dropout_rate),
 
-            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=7, padding="valid"),
+            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm1d(64),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=3),
             nn.Dropout(dropout_rate),
 
             nn.Flatten(),
 
-            nn.Linear(6080, 256),
+            nn.Linear(64 * 98, 256),
             nn.ReLU(),
             nn.Dropout(dropout_rate),
 
