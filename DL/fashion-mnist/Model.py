2cfe0c9923f152c3bc4e172f583f8fe96fdaf4d1
Dominik <zawlockidominik@gmail.com>
2026-06-22 21:45:35 +0200

    fashion-mnist: change hparams_optim to regular python files due to slowdown problems with notebook, save best params

--- DL/fashion-mnist/Model.py
+++ DL/fashion-mnist/Model.py
@@ -1,19 +1,44 @@
 import pytorch_lightning as pl
 import torch
 import torchmetrics
-from sklearn.metrics import accuracy_score
+import torchvision
 from torch import nn
 
 
 class Model(pl.LightningModule):
-    def __init__(self, n_hidden: int, n_width: int, out_features: int, lr: float):
+    def __init__(self, fold: int, dropout_rate: float, n_hidden: int, n_width: int, out_features: int, lr: float):
         super().__init__()
         self.save_hyperparameters()
 
-        self.model = nn.Sequential(
-            nn.Linear(28 * 28, n_width),
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.Linear(n_width, out_features),
+            nn.MaxPool2d(2),
+            nn.Dropout(dropout_rate),
+            # 14x14
+
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Dropout(dropout_rate),
+            # 7x7
+        )
+        linear = [
+            nn.Flatten(),
+            nn.Linear(32 * 7*7, n_width),
+            nn.ReLU(),
+        ]
+        for i in range(n_hidden):
+            linear.append(nn.Linear(n_width, n_width))
+            linear.append(nn.ReLU())
+        linear.append(nn.Linear(n_width, out_features))
+        self.fully_connected = nn.Sequential(*linear)
+
+        self.model = nn.Sequential(
+            self.conv,
+            self.fully_connected,
         )
 
         self.loss_fn = nn.CrossEntropyLoss()
@@ -21,7 +46,6 @@ class Model(pl.LightningModule):
         self.accuracy = torchmetrics.Accuracy("multiclass", num_classes=out_features)
 
     def forward(self, x):
-        x = x.reshape(-1, 28 * 28)  # for linear input layer
         return self.model(x)
 
     def training_step(self, batch, batch_idx):
@@ -32,7 +56,12 @@ class Model(pl.LightningModule):
         self.log_dict({
             "train_loss": loss,
             "train_accuracy": accuracy,
-        }, on_step=False, on_epoch=True)
+        }, on_step=False, on_epoch=True, prog_bar=True)
+
+        if batch_idx % 100 == 0:
+            grid = torchvision.utils.make_grid(x[:64])
+            self.logger.experiment.add_image("images", grid, self.global_step)
+
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -43,7 +72,7 @@ class Model(pl.LightningModule):
         self.log_dict({
             "val_loss": loss,
             "val_accuracy": accuracy,
-        }, on_step=False, on_epoch=True)
+        }, on_step=False, on_epoch=True, prog_bar=True)
         return loss
 
     def test_step(self, batch, batch_idx):
---
+++ DL/fashion-mnist/hparams_optim.py
@@ -0,0 +1,54 @@
+import json
+
+from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
+from pytorch_lightning.trainer.trainer import Trainer
+import optuna
+
+from Model import Model
+from Datamodule import Datamodule
+
+OUT_FEATURES = 10
+
+datamodule = Datamodule()
+
+logger = TensorBoardLogger("runs", name="hparams")
+
+
+def objective(trial):
+    dropout_rate = trial.suggest_float("dropout_rate", 0, 0.4)
+    n_hidden = trial.suggest_int("n_hidden", 1, 5)
+    n_width = trial.suggest_categorical("n_width", [16, 32, 64, 128, 256])
+    lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
+
+    total_score = 0.0
+    for fold, train_loader, val_loader in datamodule.cv_train_val_splits():
+        print(f"Starting new training {trial.number=} {fold=}")
+
+        model = Model(fold, dropout_rate, n_hidden, n_width, OUT_FEATURES, lr)
+        trainer = Trainer(
+            max_epochs=5,
+            fast_dev_run=False,
+            enable_checkpointing=False,
+            enable_progress_bar=False,
+            logger=logger,
+        )
+        trainer.fit(model, train_loader, val_loader)
+        result = trainer.validate(model, val_loader)
+
+        total_score += result[0]["val_accuracy"]
+
+    logger.log_hyperparams(trial.params, result[0])
+
+    avg_score = total_score / 5
+
+    return avg_score
+
+
+study = optuna.create_study(study_name="hparams", direction="maximize")
+study.optimize(objective, n_trials=3)
+
+with open("best_params.json", "w") as f:
+    params = study.best_params.copy()
+    params["score"] = study.best_value
+    s = json.dumps(params, indent=4)
+    f.write(s)
